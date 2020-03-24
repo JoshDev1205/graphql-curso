a94bd543434c5bebe17234b37c58694e7728c470
Joseph Ospina Tafur <josh.999126@gmail.com>
2020-03-24 15:20:32 -0500

    capitulo 1

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => `Hello World`,
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(() => console.log('Server is running on localhost:4000'))
